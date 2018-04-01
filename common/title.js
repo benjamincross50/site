7a9e8c30fc27185aced7d295ef9eaa3992ceb74f
benjamincross50 <benjamincross50@gmail.com>
2018-04-01 18:40:35 +0100

    second commit added touch events

--- common/title.js
+++ common/title.js
@@ -1,7 +1,7 @@
 document.write("\
-<div class='logo'><img src='../../img/blue_oct.png' alt='octree logo'> </div>\
+<div class='logo'><img src='../../img/gal.png' alt='octree logo'> </div>\
                 <div class='title-text'><h1 align='middle'>Manufacturing Shape</h1>\
                     <p align='center'>Dr. Benjamin Cross</p></div>\
-                <div class='logo'><img src='../../img/blue_oct.png' alt='octree logo'> </div>\
+                <div class='logo'><img src='../../img/gar.png' alt='octree logo'> </div>\
 ");
 
--- pages/curves/js/gl_bezier.js
+++ pages/curves/js/gl_bezier.js
@@ -27,19 +27,6 @@ function main(){
         return;
     }
      
-    /*var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if(a_Position<0){
-        console.log('Failed to find a_Position');
-        return;
-    }
-    
-    gl.vertexAttrib3f(a_Position,0.0,0.0,0.0);//*/
-    
-    //var n = initVertexBuffers(gl);
-    //if(n<0){
-    //    console.log('Failed to set the positions of the vertices');
-    //    return;
-    //}
     
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -52,7 +39,6 @@ function main(){
     // Bind buffer object to target
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     
-    //canvas.onmousedown=function (ev) { click(ev,gl,canvas) };
     canvas.onmousedown=function (ev) { mousedown=true; evX=ev.clientX; evY=ev.clientY; rect= ev.target.getBoundingClientRect();};
     canvas.onmousemove=function (ev) { if(mousedown===true) { evX=ev.clientX; evY=ev.clientY; rect= ev.target.getBoundingClientRect();}};
     canvas.onmouseup=function (ev) { mousedown=false; };
@@ -73,57 +59,52 @@ function main(){
     
 }
 
-function initVertexBuffers(gl){
-    var vertices = new Float32Array ([
-       -0.5,0.0, 0.5, -0.0,  
-    ]);
-    var n=2;
-    // Create a buffer object
-    var vertexBuffer=gl.createBuffer();
-    if(!vertexBuffer){
-        console.log('Failed to create the buffer object');
-        return -1;
-    }
-    // Bind buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // Write data into the buffer object
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if(a_Position<0){
-        console.log('Failed to find a_Position');
-        return;
-    }
-    // assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_Position);
-    
-    return n;
-    
-}
 var vertices = new Float32Array ([
-       -0.5,0.0, 0.5, -0.0,  0.0,0.0
+       -0.5,-0.5, 0.0, 0.5,  0.5,-0.5, 0.0,0.0
     ]);
 var g_points=[];
 var mousedown=false;
 var evX=0,evY=0;
 var rect ;
 
-function click(ev,gl,canvas){
-    console.log('Click registered');  
+
+
+
+function draw(gl,canvas){
+    
+    if(mousedown)
+    {
+        var x=((evX-rect.left)-canvas.width/2.0)/(canvas.width/2.0);
+        var y=(canvas.height/2.0-(evY-rect.top))/(canvas.height/2.0);
+
+        var dist1=Math.abs(vertices[0]-x)+Math.abs(vertices[1]-y);
+        var dist2=Math.abs(vertices[2]-x)+Math.abs(vertices[3]-y);
+        var dist3=Math.abs(vertices[4]-x)+Math.abs(vertices[5]-y);
+        if(dist1<0.2){
+            vertices[0]=x;
+            vertices[1]=y;
+        } else if(dist2<0.2)
+        {
+            vertices[2]=x;
+            vertices[3]=y;
+        } else if(dist3<0.2)
+        {
+            vertices[4]=x;
+            vertices[5]=y;
+        }
+        
+        console.log('x: '+x+', y: '+y);  
+    }
     
     
-    var x=ev.clientX;
-    var y=ev.clientY;
-   
+    var slider = document.getElementById("myRange");
+    var t=slider.value/100.0;
+    vertices[6]=vertices[0]*(1-t)*(1-t)+vertices[2]*t*(1-t)*2+vertices[4]*t*t;
+    vertices[7]=vertices[1]*(1-t)*(1-t)+vertices[3]*t*(1-t)*2+vertices[5]*t*t;
     
-    x=((x-rect.left)-canvas.width/2.0)/(canvas.width/2.0);
-    y=(canvas.height/2.0-(y-rect.top))/(canvas.height/2.0);
     
-    vertices[0]=x;
-    vertices[1]=y;
-    console.log('x: '+x+', y: '+y);  
     
+    //mousedown=false;
     // Write data into the buffer object
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW);
     
@@ -138,61 +119,9 @@ function click(ev,gl,canvas){
     
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS,0,2);
+    gl.drawArrays(gl.POINTS,0,4);
     gl.lineWidth(2);
-    gl.drawArrays(gl.LINES,0,2);
-    
-    // is mouse up?
-    //canvas.onmouseup
-}
-
-function draw(gl,canvas){
-    
-    if(mousedown)
-    {
-        var x=((evX-rect.left)-canvas.width/2.0)/(canvas.width/2.0);
-        var y=(canvas.height/2.0-(evY-rect.top))/(canvas.height/2.0);
-
-        var dist1=Math.abs(vertices[0]-x)+Math.abs(vertices[1]-y);
-        var dist2=Math.abs(vertices[2]-x)+Math.abs(vertices[3]-y);
-        if(dist1<0.2){
-            vertices[0]=x;
-            vertices[1]=y;
-        } else if(dist2<0.2)
-        {
-            vertices[2]=x;
-            vertices[3]=y;
-        }
-        
-        console.log('x: '+x+', y: '+y);  
-    }
-    
-    
-    var slider = document.getElementById("myRange");
-    var t=slider.value/100.0;
-    vertices[4]=vertices[0]*(1-t)+vertices[2]*t;
-    vertices[5]=vertices[1]*(1-t)+vertices[3]*t;
-    
-    
-    
-    //mousedown=false;
-    // Write data into the buffer object
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if(a_Position<0){
-        console.log('Failed to find a_Position');
-        return;
-    }
-    // assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_Position);
-    
-    gl.clearColor(0.0,0.0,0.0,1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS,0,3);
-    gl.lineWidth(15);
-    gl.drawArrays(gl.LINES,0,2);
+    gl.drawArrays(gl.LINE_STRIP,0,3);
     
     
 }
