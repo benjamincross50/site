b14120a282436d847183648becf5e53b7e81b0f3
benjamincross50 <benjamincross50@gmail.com>
2018-04-01 17:25:05 +0100

    site 2.0 from PC

---
+++ common/nav.js
@@ -0,0 +1,10 @@
+document.write("\
+<ul>\n\
+<li><a href='../../index.html'>Home</a></li>\n\
+<li><a href='../../pages/projects/projects.html'>Projects</a></li>\n\
+<li><a href='../../pages/applets/applets.html'>Applets</a></li>\n\
+<li><a href='../../pages/pubs/pubs.html'>Publications</a></li>\n\
+<li><a href='../../pages/bio/bio.html'>Biography</a></li>\n\
+</ul>\n\
+");
+
---
+++ common/socket.js
@@ -0,0 +1,4 @@
+document.write("\
+<div class='socket'>Written in HTML, CSS, JavaScript, WebGL and GLSL.</div>\
+");
+
